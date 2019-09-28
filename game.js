ee0a19e7a4d202e38cd19594889d96be029451bc
Joseph Jabbar <jabbarman@gmail.com>
2019-09-28 18:13:37 +0100

    added Hi-Score Scene

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 
 const width = 800;
 const height = 600;
-const starsPerWave = 10; // NB the actual no will be 1 more as it creates the 1st one and then repeats by this value
 
+let debug = true;
+
+let starsPerWave = 10; // NB the actual no will be 1 more as it creates the 1st one and then repeats by this value
 let lives = 3;
 let score = 0;
 let hiScore = 100;
@@ -21,6 +23,12 @@ let livesText;
 let gameOverText;
 let music;
 
+if (debug) {
+    starsPerWave = 0;
+    lives = 1;
+    hiScore = 0;
+}
+
 class SceneA extends Phaser.Scene {
 
     constructor ()
@@ -38,7 +46,7 @@ class SceneA extends Phaser.Scene {
         let background = this.add.sprite(0, 0, 'background_image');
         background.setOrigin(0,0);
         this.add.text(150, 230, 'The Dude', { fontSize: '100px', color: '#0000FF' });
-        this.input.on('pointerup', function (pointer) {
+        this.input.on('pointerup', function () {
             this.scene.start('SceneB');
         }, this);
 
@@ -205,7 +213,7 @@ class SceneB extends Phaser.Scene {
             // show game over text
             this.gameOverText.visible = true;
 
-            this.input.on('pointerup', function (pointer) {
+            this.input.on('pointerup', function () {
                 this.scene.start('SceneC');
             }, this);
         }
@@ -264,6 +272,139 @@ class SceneC extends Phaser.Scene {
         background.setOrigin(0,0);
         this.add.text(180, 200, 'The End', { fontSize: '100px', color: '#0000FF' });
         this.add.text(180, 350, 'High Score ' + score, { fontSize: '50px', color: '#0000FF' });
+        this.input.on('pointerup', function () {
+            this.scene.start('SceneD');
+        }, this);
+    }
+}
+
+class SceneD extends Phaser.Scene {
+    constructor ()
+    {
+        super('SceneD');
+    }
+
+    preload() {
+        this.load.image('block', 'assets/input/block.png');
+        this.load.image('rub', 'assets/input/rub.png');
+        this.load.image('end', 'assets/input/end.png');
+        this.load.bitmapFont('arcade', 'assets/fonts/bitmap/arcade.png', 'assets/fonts/bitmap/arcade.xml');
+    }
+
+    create() {
+        var chars = [
+            ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+            ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'],
+            ['U', 'V', 'W', 'X', 'Y', 'Z', '.', '-', '<', '>']
+        ];
+        var cursor = {x: 0, y: 0};
+        var name = '';
+
+        var input = this.add.bitmapText(130, 50, 'arcade', 'ABCDEFGHIJ\n\nKLMNOPQRST\n\nUVWXYZ.-').setLetterSpacing(20);
+
+        input.setInteractive();
+
+        var rub = this.add.image(input.x + 430, input.y + 148, 'rub');
+        var end = this.add.image(input.x + 482, input.y + 148, 'end');
+
+        var block = this.add.image(input.x - 10, input.y - 2, 'block').setOrigin(0);
+
+        var legend = this.add.bitmapText(80, 260, 'arcade', 'RANK  SCORE   NAME').setTint(0xff00ff);
+
+        this.add.bitmapText(80, 310, 'arcade', '1ST   50000    ').setTint(0xff0000);
+        this.add.bitmapText(80, 360, 'arcade', '2ND   40000    ICE').setTint(0xff8200);
+        this.add.bitmapText(80, 410, 'arcade', '3RD   30000    GOS').setTint(0xffff00);
+        this.add.bitmapText(80, 460, 'arcade', '4TH   20000    HRE').setTint(0x00ff00);
+        this.add.bitmapText(80, 510, 'arcade', '5TH   10000    ETE').setTint(0x00bfff);
+
+        var playerText = this.add.bitmapText(560, 310, 'arcade', name).setTint(0xff0000);
+
+        this.input.keyboard.on('keyup', function (event) {
+
+            if (event.keyCode === 37) {
+                //  left
+                if (cursor.x > 0) {
+                    cursor.x--;
+                    block.x -= 52;
+                }
+            } else if (event.keyCode === 39) {
+                //  right
+                if (cursor.x < 9) {
+                    cursor.x++;
+                    block.x += 52;
+                }
+            } else if (event.keyCode === 38) {
+                //  up
+                if (cursor.y > 0) {
+                    cursor.y--;
+                    block.y -= 64;
+                }
+            } else if (event.keyCode === 40) {
+                //  down
+                if (cursor.y < 2) {
+                    cursor.y++;
+                    block.y += 64;
+                }
+            } else if (event.keyCode === 13 || event.keyCode === 32) {
+                //  Enter or Space
+                if (cursor.x === 9 && cursor.y === 2 && name.length > 0) {
+                    //  Submit
+                } else if (cursor.x === 8 && cursor.y === 2 && name.length > 0) {
+                    //  Rub
+                    name = name.substr(0, name.length - 1);
+
+                    playerText.text = name;
+                } else if (name.length < 3) {
+                    //  Add
+                    name = name.concat(chars[cursor.y][cursor.x]);
+
+                    playerText.text = name;
+                }
+            }
+
+        });
+
+        input.on('pointermove', function (pointer, x, y) {
+
+            var cx = Phaser.Math.Snap.Floor(x, 52, 0, true);
+            var cy = Phaser.Math.Snap.Floor(y, 64, 0, true);
+            var char = chars[cy][cx];
+
+            cursor.x = cx;
+            cursor.y = cy;
+
+            block.x = input.x - 10 + (cx * 52);
+            block.y = input.y - 2 + (cy * 64);
+
+        }, this);
+
+        input.on('pointerup', function (pointer, x, y) {
+
+            var cx = Phaser.Math.Snap.Floor(x, 52, 0, true);
+            var cy = Phaser.Math.Snap.Floor(y, 64, 0, true);
+            var char = chars[cy][cx];
+
+            cursor.x = cx;
+            cursor.y = cy;
+
+            block.x = input.x - 10 + (cx * 52);
+            block.y = input.y - 2 + (cy * 64);
+
+            if (char === '<' && name.length > 0) {
+                //  Rub
+                name = name.substr(0, name.length - 1);
+
+                playerText.text = name;
+            } else if (char === '>' && name.length > 0) {
+                //  Submit
+            } else if (name.length < 3) {
+                //  Add
+                name = name.concat(char);
+
+                playerText.text = name;
+            }
+
+        }, this);
     }
 }
 
@@ -338,13 +479,14 @@ let config = {
         width: width,
         height: height
     },
+    pixelArt: true,
     physics: { default: 'arcade',
         arcade: {
             gravity: { y: 300 },
             debug: false
         }
     },
-    scene: [ SceneA, SceneB, SceneC ]
+    scene: [ SceneA, SceneB, SceneC, SceneD ]
 };
 
 let game = new Phaser.Game(config);
