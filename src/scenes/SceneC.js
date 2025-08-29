d564f5a95a312c1603701d0b8ff01b08a01f6cc4
Joseph Jabbar <jabbarman@gmail.com>
2025-08-29 21:22:30 +0100

    title (SceneC) updates

--- src/scenes/SceneC.js
+++ src/scenes/SceneC.js
@@ -9,7 +9,7 @@ export class SceneC extends Phaser.Scene {
   create() {
     const background = this.add.sprite(0, 0, 'background_image');
     background.setOrigin(0, 0);
-    addTitle(this, 180, 200, 'THE END');
+    addTitle(this, 180, 200, '  THE END');
     addBig(this, 180, 300, 'YOUR SCORE ' + state.score);
     addBig(this, 180, 360, 'HIGH SCORE ' + state.hiScore);
     this.input.on('pointerup', function () {
