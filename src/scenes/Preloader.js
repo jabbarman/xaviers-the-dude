397879be5e1c7acc5561c705a9ab9ffac2c6aba0
Joseph Jabbar <jabbarman@gmail.com>
2025-09-10 21:50:23 +0100

    preloaded new music

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -27,11 +27,14 @@ export class Preloader extends Phaser.Scene {
         this.load.spritesheet('fullscreen', 'assets/fullscreen.png', { frameWidth: 64, frameHeight: 64 });
 
         // Audio
-        this.load.audio('boden', ['assets/audio/bodenstaendig_2000_in_rock_4bit.mp3', 'assets/audio/bodenstaendig_2000_in_rock_4bit.ogg']);
+        this.load.audio('boden', 'assets/audio/bodenstaendig_2000_in_rock_4bit.mp3');
         this.load.audio('tommy', 'assets/audio/tommy_in_goa.mp3');
         this.load.audio('iLoveMy8bit', 'assets/audio/i-love-my-8-bit-game-console-301272.mp3');
         this.load.audio('8BitMusic', 'assets/audio/8-bit-music-no-copyright-background-instrumental-pixel-party-322342.mp3');
-        this.load.audio('flat8bit', 'flat-8-bit-gaming-music-instrumental-211547.mp3')
+        this.load.audio('flat8bit', 'assets/audio/flat-8-bit-gaming-music-instrumental-211547.mp3')
+        this.load.audio('8bitTheme', 'assets/audio/8bit-theme-loop-chiptune-symphony-387749.mp3');
+        this.load.audio('percussiveDubstep', 'assets/audio/percussive-dubstep-chiptune-218126.mp3');
+        this.load.audio('pixelParadise', 'assets/audio/pixel-paradise-358340.mp3');
         this.load.audio('gameOver', 'assets/audio/SoundEffects/player_death.wav');
         this.load.audio('ping', 'assets/audio/SoundEffects/p-ping.mp3');
         this.load.audio('explode', 'assets/audio/SoundEffects/explosion.mp3');
