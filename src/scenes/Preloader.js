11365cde39d34a8df4e7adce854f63a7891e67b4
Joseph Jabbar <jabbarman@gmail.com>
2026-01-31 20:22:58 +0000

    feat(preloader): add loading progress bar

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -1,9 +1,55 @@
+import { WIDTH, HEIGHT } from '../config.js';
+
 export class Preloader extends Phaser.Scene {
   constructor() {
     super('Preloader');
   }
 
   preload() {
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(WIDTH / 2 - 160, HEIGHT / 2 - 30, 320, 50);
+
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', (value) => {
+      percentText.setText(`${parseInt(value * 100, 10)}%`);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(WIDTH / 2 - 150, HEIGHT / 2 - 20, 300 * value, 30);
+    });
+
+    this.load.on('complete', () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      this.scene.start('SceneA');
+    });
+
     // Fonts
     this.load.bitmapFont(
       'arcade',
@@ -76,6 +122,6 @@ export class Preloader extends Phaser.Scene {
   }
 
   create() {
-    this.scene.start('SceneA');
+    //  The 'complete' event handler will start the next scene
   }
 }
